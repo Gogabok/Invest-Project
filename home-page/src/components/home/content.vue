b6da717ceb3651ab3704be8621a4018ddf29047e
Gogabok <51056095+Gogabok@users.noreply.github.com>
2019-12-10 09:50:54 +0300

    content-component init

---
+++ home-page/src/components/home/content.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home-content">
+    <h1 class="title">Инвестирование <br/> в нейротехнологии</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home-content",
+}
+</script>
+
+<style lang="scss" scoped>
+  .home-content {
+
+    & .title {
+      font-family: "Exo 2", sans-serif;
+      font-size: 4em;
+      font-weight: bold;
+      color: #fff;
+    }
+  }
+</style>
--- home-page/src/views/Home.vue
+++ home-page/src/views/Home.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="home">
-    
+    <div class="home-wrapper">
+      <home-content
+                    class="home-content-wrapper"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-
+import homeContent from "@/components/home/content"
 export default {
   name: 'home',
   components: {
-    
+    homeContent
   }
 }
 </script>
+
+<style lang="scss">
+  .home-wrapper {
+    padding: 50px 50px 20px 50px;
+    & .home-content-wrapper {
+      max-width: 800px;
+    }
+  }
+</style>
