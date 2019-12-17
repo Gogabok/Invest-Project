591d8c97ffc8a2e1a4f926dd99f94cc52369c408
Gogabok <51056095+Gogabok@users.noreply.github.com>
2019-12-17 15:14:46 +0300

    reset

---
+++ user-panel/src/components/logo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="logo">
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'logo',
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ user-panel/src/views/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dashboard">
+    <logo></logo>
+  </div>
+</template>
+
+<script>
+import logo from '@/components/logo.vue';
+
+export default {
+  name: 'dashboard',
+  components: {
+    logo,
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
