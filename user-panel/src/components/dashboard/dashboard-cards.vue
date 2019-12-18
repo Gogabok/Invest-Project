926f630388bb7ad23bcf7e2b4e7db7322131d1e2
Gogabok <gogabok123@gmail.com>
2019-12-18 21:12:11 +0300

    dashboard-cards

---
+++ user-panel/src/components/dashboard/dashboard-cards.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dashboard-cards">
+    <dashboard-card 
+                    :balance="investBalance"
+    >
+    Инвестиции
+    </dashboard-card>
+    <dashboard-card 
+                    :balance="dividendBalance"
+                    :extraInfo="'от 5% до 15% в месяц'"
+    >
+    Дивиденды
+    </dashboard-card>
+    <dashboard-card 
+                    :balance="partherBalance"
+    >
+    Партнерство
+    </dashboard-card>
+  </div>
+</template>
+
+<script>
+import dashboardCard from "@/components/dashboard/dashboard-card"
+
+export default {
+  name: 'dashboard-cards',
+  components: {
+    dashboardCard
+  },
+  data: () => ({
+    investBalance: (0).toFixed(2),
+    dividendBalance: (0).toFixed(2),
+    partherBalance: (0).toFixed(2)
+  })
+}
+</script>
+
+<style lang="scss">
+  .dashboard-cards {
+    margin-top: 40px;
+    padding: 0px 0px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    max-width: 100%;
+    & * {
+      font-family: "Exo 2", sans-serif;
+    }
+  }
+</style>
---
+++ user-panel/src/components/header/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navigation">
+    <div class="navigation-wrapper">
+      <v-logo></v-logo>
+      <v-nav-btn :VClass="'invest'">Инвестировать</v-nav-btn>
+      <v-nav-btn :VClass="'get'">Вывести деньги</v-nav-btn>
+      <v-user-card>{{ userName }}</v-user-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import VLogo from "@/components/header/logo"
+import VNavBtn from "@/components/header/VNavBtn"
+import VUserCard from "@/components/header/VUserCard"
+export default {
+  name: 'navigation-component',
+  components: {
+    VLogo, VNavBtn, VUserCard
+  },
+  data: () => ({
+    userName: 'woohoohooh@gmail.com'
+  })
+}
+</script>
+
+<style lang="scss">
+  .navigation-wrapper {
+    padding: 20px 0px;
+    display: flex;
+    align-items: center;
+    & * {
+      font-family: "Exo 2";
+    }
+  }
+</style>
