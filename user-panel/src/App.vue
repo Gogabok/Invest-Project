bae28396ad3998b567ed48fd100a17bee2d83670
Gogabok <gogabok123@gmail.com>
2019-12-18 21:12:52 +0300

    resets and others

--- user-panel/src/App.vue
+++ user-panel/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="main-app-container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -9,3 +11,18 @@ export default {
   name: 'App',
 };
 </script>
+
+
+<style lang="scss">
+.main-app-container {
+  height: 100vh;
+  width: 100%;
+  position: relative;
+  background: #20272B;
+  background-image: url("/assets/common/main-bg.svg");
+  background-position: calc(50% - 76px) -500px;
+  background-size: 2500px;
+  background-repeat: no-repeat;
+  overflow: hidden;
+}
+</style>
