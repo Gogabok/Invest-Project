8e8bed8dba630e94608aec46971c9acc392fa43e
Gogabok <gogabok123@gmail.com>
2020-08-16 14:42:18 +0300

    adding searching input

---
+++ admin-panel/src/components/shared-components/searching.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="vue-searching">
+    <label class="vue-searching-label" for="vue-searching">
+      <input :class="isEmpty ? 'notAnim' : false" v-model="value" class="vue-searching-input" type="text">
+      <span :class="isEmpty ? 'notDisplayed' : false" class="vue-searching-placeholder">{{ placeholder }}</span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Vue-Searching',
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Найти'
+    }
+  }, 
+  data: () => ({
+    value: ''
+  }),
+  computed: {
+    isEmpty() {
+      return this.value.length > 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .vue-searching {
+    width: 300px;
+    position: relative;
+    &:after {
+      position: absolute;
+      right: 0px;
+      top: 50%;
+      transform: translate(0, -50%);
+      content: '';
+      width: 40px;
+      height: 40px;
+      background: url('../../assets/icons/search.svg') no-repeat center center;
+      background-size: 50%;
+      cursor: pointer;
+    }
+    &-label {
+      width: 100%;
+    }
+    &-placeholder {
+      position: absolute;
+      left: 5px;
+      padding: 0px 5px;
+      top: 50%;
+      transform: translate(0, -50%);
+      color: #707070;
+      font-family: 'SegoeUI';
+      font-weight: 700;
+      font-size: 1.5rem;
+      pointer-events: none;
+      display: flex;
+      align-items: center;
+      transition-duration: .2s;
+      user-select: none;
+      &.notDisplayed {
+        top: 0px;
+        background: #20272B;
+        font-size: 1rem;
+      }
+    }
+    &-input {
+      background: none;
+      border: none;
+      border-bottom: 1px solid #F4F4F4;
+      padding: 5px 35px 5px 10px;
+      width: 100%;
+      color: #fff;
+      font-family: 'SegoeUI';
+      font-weight: 700;
+      font-size: 1.5rem;
+      height: 40px;
+      &:focus {
+        padding-top: 10px;
+        border: 1px solid #F4F4F4;
+        border-radius: 4px;
+      }
+      &.notAnim:focus {
+        padding-top: 5px;
+      }
+      &.notAnim {
+        border: 1px solid #F4F4F4;
+        border-radius: 4px;
+      }
+    }
+  }
+  .vue-searching-input:focus + .vue-searching-placeholder {
+    top: 0px;
+    background: #20272B;
+    font-size: 1rem;
+  }
+</style>
--- admin-panel/src/components/shared-components/vue-nav.vue
+++ admin-panel/src/components/shared-components/vue-nav.vue
@@ -1,14 +1,24 @@
 <template>
   <nav class="main-navigation">
     <ul class="nav-list">
-      <li v-for="item in nav" :key="item.text" class="nav-list-item">{{ item.text }}</li>
+      <router-link tag="li" :to="`/${item.link}`" exact v-for="item in nav" :key="item.text" class="nav-list-item" active-class="active">{{ item.text }}</router-link>
     </ul>
+    <div class="searching">
+      <button class="searching-btn">
+        20%
+      </button>
+      <vue-searching :placeholder="`Найти пользователя`"></vue-searching>
+    </div>
   </nav>
 </template>
 
 <script>
+import vueSearching from '@/components/shared-components/searching'
 export default {
   name: 'vue-nav',
+  components: {
+    vueSearching
+  },
   data: () => ({
     nav: [
       {
@@ -35,18 +45,42 @@ export default {
 <style lang="scss" scoped>
   .main-navigation {
     border-bottom: 1px solid #F3F4F4;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0px 50px;
     & .nav-list {
       display: flex;
       align-items: center;
       &-item {
-        padding: 15px 30px;
+        padding: 20px 30px;
         border-bottom: 2px solid transparent;
         font-family: 'SegoeUI';
         font-weight: 700;
         color: #F3F4F4;
+        user-select: none;
+        transition-duration: .2s;
+        cursor: pointer;
+        font-size: 1.8rem;
         &.active {
           border-color: #F3F4F4;
         }
+      }
+    }
+    & .searching {
+      display: flex;
+      align-items: center;
+      &-btn {
+        background: #F3F4F4;
+        border: none;
+        padding: 4px 9px 8px 9px;
+        color: #555D6A;
+        font-family: 'SegoeUI';
+        font-weight: 700;
+        font-size: 1.7rem;
+        user-select: none;
+        margin-right: 15px;
       }
     }
   }
