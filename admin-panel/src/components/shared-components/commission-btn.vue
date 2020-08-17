2cb74011adf6847ff6a74532cf24615aa35e96ab
Gogabok <gogabok123@gmail.com>
2020-08-17 22:40:54 +0300

    upd

--- admin-panel/src/components/shared-components/commission-btn.vue
+++ admin-panel/src/components/shared-components/commission-btn.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="commission">
+    <input :class="isOpen ? 'open' : false" v-model="value" class="commission-input" type="text">
     <button @click="openMenu" class="commission-btn">
-      <span v-show="!isOpen">20%</span>
+      <span v-show="!isOpen">{{ value }}%</span>
       <img v-show="isOpen" class="commission-btn-ok" ondragstart="return false" src="../../assets/icons/ok-dark.svg" alt="">
     </button>
   </div>
@@ -11,7 +12,8 @@
 export default {
   name: 'commissin-btn',
   data: () => ({
-    isOpen: false
+    isOpen: false,
+    value: 20
   }),
   methods: {
     openMenu() {
@@ -24,7 +26,24 @@ export default {
 <style lang="scss" scoped>
   .commission {
     display: flex;
-    align-items: center;
+      height: 34px;
+    &-input {
+      background: transparent;
+      border: 1px solid #F3F4F4;
+      font-family: 'SegoeUI';
+      font-weight: 700;
+      color: #fff;
+      font-size: 1.7rem;
+      width: 1px;
+      text-align: center;
+      transition-duration: .2s;
+      opacity: 0;
+      &.open {
+        width: 75px;
+        padding: 4px 9px 8px 9px;
+        opacity: 1;
+      }
+    }
     &-btn {
       background: #F3F4F4;
       border: none;
@@ -36,10 +55,11 @@ export default {
       user-select: none;
       margin-right: 15px;
       transition-duration: .2s;
+      min-width: 50px;
       &-ok {
         width: 18px;
         margin-top: -4px;
-      transition-duration: .2s;
+        transition-duration: .2s;
       }
     }
   }
